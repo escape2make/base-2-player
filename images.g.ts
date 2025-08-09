43a27c63a8a9ece0dac4c4b1d66e2ede99651aa1
Escape2Make Makers Club <e2m.makers@gmail.com>
2025-08-09 12:32:36 +0100

    Update main.blocks, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,60 +3,7 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "EF4U/irD157-Q]QK:{Ji":
-            case "left":return img`
-..cccc.........
-.c5553c........
-c35553ccccccc..
-c35553c355555c.
-c35553c5555533c
-c35553c3333333c
-c35553cccccc33c
-c35553c55553ccc
-c35553c555553c.
-c35553c555553c.
-c35553c555553c.
-c35553c555553c.
-c35553c555553c.
-c35553c555553c.
-c35553c555553c.
-c35533c555553c.
-c33333cccccccc.
-c33333c355555c.
-c33333c5555533c
-c33333c3333333c
-c33333c3333333c
-c33333c3333333c
-.cccccccccccccc
-.cbbc.....cbbc.
-`;
-            case "Y7HDx_!nF+bD?`7ig8lC":
-            case "right":return img`
-.........cccc..
-........c6777c.
-..ccccccc67776c
-.c777776c67776c
-c6677777c67776c
-c6666666c67776c
-c66cccccc67776c
-ccc67777c67776c
-.c677777c67776c
-.c677777c67776c
-.c677777c67776c
-.c677777c67776c
-.c677777c67776c
-.c677777c67776c
-.c677777c67776c
-.c677777c66776c
-.cccccccc66666c
-.c777776c66666c
-c6677777c66666c
-c6666666c66666c
-c6666666c66666c
-c6666666c66666c
-cccccccccccccc.
-.cbbc.....3bbc.
-`;
+
         }
         return null;
     })
